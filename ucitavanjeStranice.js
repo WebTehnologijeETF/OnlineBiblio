b88b63bffe7ce1d145bda69e2129830071d71208
Anes Luckin <aluckin1@etf.unsa.ba>
2015-09-03 18:00:54 +0200

    Spirala 3 konacna verzija

---
+++ ucitavanjeStranice.js
@@ -0,0 +1,28 @@
+function loadPage(page)
+{
+
+	var x=new XMLHttpRequest();
+
+	x.onreadystatechange=function()
+	{
+
+	if(x.readyState==4 && x.status==200)
+	{
+	
+		document.open();
+				
+		document.write(x.responseText);
+						
+		document.close();
+					
+	}
+			
+	}	
+				
+	x.open("GET", page, true);
+	
+	x.send();
+
+  
+}
+
