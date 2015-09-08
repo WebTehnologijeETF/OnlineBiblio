19770aca3945c260c5a78178fe6069e79c54151c
Anes Luckin <aluckin1@etf.unsa.ba>
2015-09-08 18:59:35 +0200

    Spirala 5 update

--- ucitavanjeStranice.js
+++ ucitavanjeStranice.js
@@ -2,20 +2,22 @@ function loadPage(page)
 {
 
 	var x=new XMLHttpRequest();
-	
+
 	x.onreadystatechange=function()
 	{
 
-	if(x.readyState==4 && x.status==200){
+	if(x.readyState==4 && x.status==200)
+	{
 	
 		document.open();
 				
 		document.write(x.responseText);
 						
 		document.close();
+					
 	}
 			
-	}
+	}	
 				
 	x.open("GET", page, true);
 	
